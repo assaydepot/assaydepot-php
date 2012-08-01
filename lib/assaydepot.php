a88940df5a79511168fabb25ba64019feaa67ee0
Gremlyn <gremlyn1@gmail.com>
2012-08-01 09:26:30 -0700

    Added class methods for functionality. Need to decide how to accept and parse facets.

--- lib/assaydepot.php
+++ lib/assaydepot.php
@@ -1,6 +1,39 @@
 <?php
 
+class AssayDepot {
 
+    private $auth_token;
+    private $url;
 
+    private function __construct($auth_token, $url) {
+        $this->auth_token = $auth_token;
+        $this->url = $url;
+    }
+
+    function search_url($params) {
+        $format = '%s/%s.json?';
+        $format = trim(str_repeat("%s=%s&", count($params)-2), "&");
+        return vsprintf($format, $params);
+    }
+
+    function get_url($params) {
+        $format = '%s/%s/%s.json?';
+        $format = trim(str_repeat("%s=%s&", count($params)-3), "&");
+        return vsprintf($format, $params);
+    }
+
+    function search($search_type, $query, $facets) {
+        $params = array($this->url, $search_type, 'q', $query);
+        //sort out facets here
+        array_push($params, "auth_token", $this->$auth_token);
+    }
+
+    function get($search_type, $query, $id, $facets) {
+        $params = array($this->url, $search_type, $id, 'q', $query);
+        //sort out facets here
+        array_push($params, "auth_token", $this->$auth_token);
+    }
+
+}
 
 ?>
